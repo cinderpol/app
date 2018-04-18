2fca43a1f5f299393b906faa13a5a0b3224cefc5
cinderpol <cinderpol@gmail.com>
2018-04-18 13:51:42 +0800

    Base install

---
+++ src/main.js
@@ -0,0 +1,26 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
+import { store } from './store'
+import VueSweetalert2 from 'vue-sweetalert2'
+const Parse = require('parse')
+
+Parse.initialize('appid')
+Parse.serverURL = 'http://192.168.189.135:80/parse'
+
+Vue.use(Vuetify, VueSweetalert2)
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  store,
+  components: { App },
+  template: '<App/>'
+})
